bc17dbdd497ff7e37e2e88fa8fcae7869431706e
darlan <darlan@darlan-S430-G-BE41P1>
2019-10-11 06:38:00 -0300

    Versão com SSE criada a classe SSE incluindo programa de teste.

---
+++ TestesSSE/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=testessse
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/testessse
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=testessse.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/testessse.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=testessse
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/testessse
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=testessse.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/testessse.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
